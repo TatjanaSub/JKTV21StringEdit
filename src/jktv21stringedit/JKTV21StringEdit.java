2fceeefc51ad2605c043d526cc4a2609e9463982
TatjanaSub <tatjanasubbotina@ivkhk.ee>
2022-09-21 21:09:50 +0300

    4 funktsia

--- src/jktv21stringedit/JKTV21StringEdit.java
+++ src/jktv21stringedit/JKTV21StringEdit.java
@@ -21,13 +21,14 @@ public class JKTV21StringEdit {
         System.out.println("Vvedite text");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+        
         do{
             System.out.println("Funktsii programmq:");
             System.out.println("0. Vqhod iz programmq");
             System.out.println("1. Vqvesti kol-vo simvilov");
             System.out.println("2. Vqvesti kol-vo slov");
             System.out.println("3. Naiti slovo v stroke");
-            System.out.println("4. zamenit ukazannoe slovo v stroke");
+            System.out.println("4. Zamenit' ukazannoe slovo v stroke");
             System.out.println("Vqberite nomer funktsii: ");
             int number = scanner.nextInt();
             scanner.nextLine();
@@ -46,12 +47,24 @@ public class JKTV21StringEdit {
                 String subStr = scanner.nextLine();
                 int index = str.indexOf(subStr);
                 if(index < 0){
-                    
+                    System.out.println("Takogo slova net v stroke");
                 }else{
                     index++;
+                    System.out.println("Ukazannoe slovo nahoditsja na " + index + " pozitsii");
                 }
             }else if(number == 4){
                 System.out.println("Vq vqbrali punkt " + number);
+                System.out.println("Vvede slovo, kotoroe nado zamenit'");
+                String strOld = scanner.nextLine();
+                int index = str.indexOf(strOld);
+                if(index < 0){
+                    System.out.println("Takogo slova v stroke net");
+                }else{
+                    System.out.println("Vvedite slovo na kotoroe zamenjaem: ");
+                    String strNew = scanner.nextLine();
+                    System.out.println("Izmenennaja stroka:");
+                    System.out.println(str.replace(strOld, strNew));
+                }
             }
             
         }while(true);
